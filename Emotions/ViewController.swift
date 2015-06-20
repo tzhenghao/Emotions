26d9fc6cc0afa852ab115be0d5ba827b945132a8
Zheng Hao Tan <tanzhao@umich.edu>
2015-06-20 18:21:16 -0500

    Initial Commit

---
+++ Emotions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emotions
+//
+//  Created by Zheng Hao Tan on 6/20/15.
+//  Copyright (c) 2015 Zheng Hao Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
