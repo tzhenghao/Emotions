53d9e72263d41ac5793962c4b2b81c6bd07fb35d
Zheng Hao Tan <tanzhao@umich.edu>
2015-06-20 18:39:32 -0500

    Added FaceView

---
+++ Emotions/FaceView.swift
@@ -0,0 +1,20 @@
+//
+//  FaceView.swift
+//  Emotions
+//
+//  Created by Zheng Hao Tan on 6/20/15.
+//  Copyright (c) 2015 Zheng Hao Tan. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /* 
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+}
