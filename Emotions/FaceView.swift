5c207a000c815a03d465ed9b10509fba644c5d24
Zheng Hao Tan <tanzhao@umich.edu>
2015-06-20 18:57:37 -0500

    Added face and fixed scaling and rotating issues

--- Emotions/FaceView.swift
+++ Emotions/FaceView.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class FaceView: UIView {
 
-    /* 
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+    var lineWidth: CGFloat = 3 { didSet { setNeedsDisplay() } }
+    
+    var color: UIColor = UIColor.blueColor() { didSet { setNeedsDisplay() } }
+
+    var scale: CGFloat = 0.9 { didSet { setNeedsDisplay() } }
+
+    var faceCenter: CGPoint {
+        return convertPoint(center, fromView: superview)
     }
-    */
+    
+    var faceRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height)/2 * scale
+    }
+    
+
+    override func drawRect(rect: CGRect) {
+        let facePath = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+        
+        facePath.lineWidth = lineWidth
+        color.set()
+        facePath.stroke()
+    }
 }
