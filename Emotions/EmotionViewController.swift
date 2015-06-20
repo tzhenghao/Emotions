04be7702f418afed06830aa115c0a018a2e489e2
Zheng Hao Tan <tanzhao@umich.edu>
2015-06-20 18:29:21 -0500

    Initial Commit

---
+++ Emotions/EmotionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EmotionViewController.swift
+//  Emotions
+//
+//  Created by Zheng Hao Tan on 6/20/15.
+//  Copyright (c) 2015 Zheng Hao Tan. All rights reserved.
+//
+
+import UIKit
+
+class EmotionViewController: UIViewController {
+
+}
