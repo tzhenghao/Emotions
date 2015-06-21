5f4e0db8efa4e8a5a2f494c03644bd7c47a484c1
Zheng Hao Tan <tanzhao@umich.edu>
2015-06-20 19:22:26 -0500

    Added eyes

--- Emotions/FaceView.swift
+++ Emotions/FaceView.swift
@@ -24,6 +24,42 @@ class FaceView: UIView {
         return min(bounds.size.width, bounds.size.height)/2 * scale
     }
     
+    // Constant ratios.
+    private struct Scaling {
+        static let FaceRadiusToEyeRadiusRatio: CGFloat = 10
+        static let FaceRadiusToEyeOffsetRatio: CGFloat = 3
+        static let FaceRadiusToEyeSeparationRatio: CGFloat = 1.5
+        static let FaceRadiusToMouthWidthRatio: CGFloat = 1
+        static let FaceRadiusToMouthHeightRatio: CGFloat = 3
+        static let FaceRadiusToMouthOffsetRatio: CGFloat = 3
+    }
+
+    private enum Eye { case Left, Right }
+    
+    // REQUIRES: A left or right eye.
+    // EFFECTS: Returns the path for the left or right eye.
+    private func bezierPathForEye(whicheye: Eye) -> UIBezierPath {
+        
+        let eyeRadius = faceRadius / Scaling.FaceRadiusToEyeRadiusRatio
+        let eyeVerticalOffset = faceRadius / Scaling.FaceRadiusToEyeOffsetRatio
+        let eyeHorizontalSeparation = faceRadius / Scaling.FaceRadiusToEyeSeparationRatio
+        
+        var eyeCenter = faceCenter
+        
+        eyeCenter.y -= eyeVerticalOffset
+        
+        switch whicheye {
+        case .Left:
+            eyeCenter.x -= eyeHorizontalSeparation/2
+        case .Right:
+            eyeCenter.x += eyeHorizontalSeparation/2
+        }
+        
+        let path = UIBezierPath(arcCenter: eyeCenter, radius: eyeRadius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+        path.lineWidth = lineWidth
+        
+        return path
+    }
 
     override func drawRect(rect: CGRect) {
         let facePath = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
@@ -31,5 +67,14 @@ class FaceView: UIView {
         facePath.lineWidth = lineWidth
         color.set()
         facePath.stroke()
+        
+        // Draw the eyes.
+        bezierPathForEye(.Left).stroke()
+        bezierPathForEye(.Right).stroke()
+        
+        // Draw the smile.
+        //let initialSmile = 0.85
+        //let smilePath = bezierPathForSmile(initialSmile)
+        //smilePath.stroke()
     }
 }
